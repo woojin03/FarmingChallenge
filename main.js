90d4456a452c2774919dfd5df2c5e567a849eea9
woojin03 <105140943+woojin03@users.noreply.github.com>
2023-08-07 21:18:36 +0900

    📌6일 차 미션 : 이미지 슬라이드 (캐러셀) 만들기 1
    <요청사항>
    💡n개의 이미지가 슬라이드 될 수 있는 영역을 만듭니다.
    💡다양한 크기의 이미지를 넣어도 별도 수정 없이 동작이 가능합니다.
    💡슬라이드 양 옆에 화살표가 있습니다.
    💡화살표를 클릭하면 슬라이드가 작동됩니다

--- main.js
+++ main.js
@@ -1,94 +1,10 @@
-const yearElement = document.querySelector('.year');
-const monthElement = document.querySelector('.month');
-const datesElement = document.querySelector('.dates');
-const memoOverlay = document.querySelector('.memo-overlay');
-const memoContainer = document.querySelector('.memo-container');
-const memoDateElement = document.querySelector('.memo-date');
-const memoInputElement = document.querySelector('.memo-input');
-
-let currentDate = new Date();
-let memoData = {};
-
-function updateCalendar() {
-    const year = currentDate.getFullYear();
-    const month = currentDate.toLocaleString('default', { month: 'long' });
-
-    yearElement.textContent = year;
-    monthElement.textContent = month;
-
-    datesElement.innerHTML = '';
-
-    const firstDay = new Date(year, currentDate.getMonth(), 1);
-    const lastDay = new Date(year, currentDate.getMonth() + 1, 0);
-
-    const startDayOfWeek = firstDay.getDay();
-
-    for (let i = 0; i < startDayOfWeek; i++) {
-        const emptyDate = document.createElement('div');
-        emptyDate.className = 'date empty';
-        datesElement.appendChild(emptyDate);
-    }
-
-    for (let date = 1; date <= lastDay.getDate(); date++) {
-        const dateElement = document.createElement('div');
-        dateElement.textContent = date;
-        dateElement.className = 'date';
-        dateElement.setAttribute('onclick', `showMemoForm(${date})`);
-
-        const dateString = getDateString(currentDate, date);
-        if (memoData[dateString]) {
-            dateElement.classList.add('memo-exists');
-        }
-
-        datesElement.appendChild(dateElement);
-    }
-}
-
-function prevM() {
-    currentDate.setMonth(currentDate.getMonth() - 1);
-    updateCalendar();
-}
-
-function nextM() {
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    updateCalendar();
-}
-
-function getDateString(dateObj, day) {
-    const year = dateObj.getFullYear();
-    const month = dateObj.getMonth() + 1;
-    return `${month}/${day}`;
-}
-
-function showMemoForm(day) {
-    const dateString = getDateString(currentDate, day);
-
-    memoOverlay.style.display = 'block';
-    memoContainer.style.display = 'block';
-    memoDateElement.textContent = dateString;
-
-    if (memoData[dateString]) {
-        memoInputElement.value = memoData[dateString];
-    } else {
-        memoInputElement.value = '';
-    }
-}
-
-function closeMemo() {
-    memoOverlay.style.display = 'none';
-    memoContainer.style.display = 'none';
-}
-
-function saveMemo() {
-    const dateString = memoDateElement.textContent;
-    const memo = memoInputElement.value.trim();
-
-    if (memo !== '') {
-        memoData[dateString] = memo;
-        updateCalendar();
-    }
-
-    closeMemo();
-}
-
-updateCalendar();
+$(document).ready(function() {
+    // 이미지 슬라이더 설정
+    $('.imgs').slick({
+      dots: true, // 페이지 점 표시
+      autoplay: false, // 자동 슬라이드 비활성화
+      prevArrow: $('.prev'), // 이전 버튼 클래스
+      nextArrow: $('.next'), // 다음 버튼 클래스
+    });
+  });
+  
